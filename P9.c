cf4e470a18bdfebbb8f42f9c3ae070c9ff87dc38
myllwnaoow <37547632+myllwnaoow@users.noreply.github.com>
2018-07-09 16:48:40 -0300

    Update P9.c

--- P9.c
+++ P9.c
@@ -1,100 +1,106 @@
-/*Myllena Gonçalves Neto
+/*Myllena GonÃ§alves Neto
 11721ETE006*/
 
-#include <stdio.h>
 #include <string.h>
-#include  <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Tamanhos
+struct dimensao
 {
-	flutuar largura;
-	flutuar a profundidade;
-	flutuante altura;
+	float largura;
+	float profundidade;
+	float altura;
 };
 
 struct produto
 {
-	char numero [ 101 ];
-	caractere nomo [ 64 ];
-	flutuante preco;
-	flutuar dimensoes;
-	struct Tamanhos final;
+	char nome[64];
+	float preco;
+	struct dimensao end;
 };
-
-estrutura de produtos [ 100 ];
-
-void  cadastrar_produto ()
-{
-	struct produto;
-	int i = 0 ;
-	printf ( " \ n \ n Numero do produto um ser cadastrado: " );
-	scanf ( " % s " , & produtos [i]. numero );
-	printf ( " \ n Nome: " );
-	scanf ( " % s " , & produtos [i]. nome );
-	getchar ();
-	printf ( " \ n Digite o preco: " );
-	scanf ( " % f " , & produtos [i]. preco );
-	getchar ();
-	printf ( " \ n Digite a largura do produto: " );
-	scanf ( " % f " , & produtos [i]. end . largura );
-	getchar ();
-	printf ( " \ n Digite a produndidade do produto: " );
-	scanf ( " % f " , & produtos [i]. end . depth );
-	getchar ();
-	printf ( " \ n Digite a altura do produto: " );
-	scanf ( " % f " , & produtos [i]. end . altura );
-	printf ( " \ n \ n Produto % s cadastrado com sucesso! \ n \ n " , produtos [i]. numero );
+void cadastro(struct produto vet_produto[], int i){
+	printf("\nDigite o nome:");
+	fgets(vet_produto[i].nome, 64, stdin);
+	printf("\nDigite o preco:");
+	scanf("%f", &vet_produto[i].preco);
+	getchar();
+	printf("\nDigite a largura:");
+	scanf("%f", &vet_produto[i].end.largura);
+	getchar();
+	printf("\nDigite a profundidade:");
+	scanf("%f", &vet_produto[i].end.profundidade);
+	getchar();
+	printf("\nDigite a altura:");
+	scanf("%f", &vet_produto[i].end.altura);
+	getchar();
 }
-
-void  export_produto ()
-{
-	char numero_consulta [ 101 ];
-	int i = 0 , sinalizador = 0 ;
-	printf ( " \ n \ n Digite o número do produto: " );
-	fflush (stdin);
-	Obtém (numero_consulta);
-	
-	para (i = 0 ; i < 101 ; i ++)
-	{
-		if ( strcmp (numero_consulta, produtos [i]. numero ) == 0 )
-		{
-			flag = 1 ;
-			printf ( " \ n \ n % s , R $ % .2f , " , produtos [i]. nome , produtos [i]. preco );
-			printf ( " L: % .2f m x P: % .2f m x R: % .2f m \ n \ n " , Produtos [i]. final . Largura , Produtos [i]. final . profundidade , Produtos [i]. end . altura );
-			pausa ;
-		}
+void pesquisa(struct produto vet_produto[], int i){
+	char condicional[64];
+	strcpy(condicional, " ");
+	if(strcmp(condicional, vet_produto[i].nome) == 0){
+		printf("\nProduto nao cadastrado!");
 	}
-	if (flag == 0 )
-	{
-		printf ( " \ n \ n Produto nao cadastrado! \ n \ n " );
-		retorno ;
+	else{
+		printf("\n %s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm",vet_produto[i].nome, vet_produto[i].preco, vet_produto[i].end.largura, vet_produto[i].end.profundidade, vet_produto[i].end.altura);
 	}
 }
-
-int  main ()
+int main()
 {
-	int opc;
-	int i = 0 ;
-	estrutura do produto produtos;
-	
-	Faz
+	struct produto vet_produto[2];
+	int opcao, produto, num_produto = 0;
+	strcpy(vet_produto[0].nome, " ");
+	strcpy(vet_produto[1].nome, " ");
+	do{
+	printf("\n\n1 - Cadastro\n2 - Consulta\n3 - Sair\n\nDigite uma opcao: ");
+	scanf("%d", &opcao);
+	getchar();  
+	switch(opcao)
 	{
-	printf ( " Menu: \ n \ n 1 - Cadastro do produto. \ n 2 - Consultar um produto. \ n 3 - Sair " );
-	printf ( " \ n \ n Digite uma opcao desejada: " );
-	scanf ( " % d " , & opc);
-	getchar ();
-		interruptor (opc)
-		{
-			caso  1 :
-				cadastrar_produto ();
-				pausa ;
-			caso  2 :
-				consultar_produto ();
-				pausa ;
-			caso  3 :
-				return  0 ;
-		}
-	} while (opc! = 4 );
-	return  0 ;	
+		case 1: 
+			printf("\nEscolha o produto 1 ou 2:");
+			scanf("%d", &produto);
+			getchar();
+			switch(produto){
+				case 1:
+					num_produto = produto -1;
+					cadastro(vet_produto, num_produto);
+					printf("\nProduto 1 cadastrado com sucesso");
+					num_produto = 0;
+					break;
+				case 2:
+					num_produto = produto -1;
+					cadastro(vet_produto, num_produto);
+					printf("\nProduto 2 cadastrado com sucesso");
+					num_produto = 0;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 2:	
+			printf("\nEscolha o produto 1 ou 2:");
+			scanf("%d", &produto);
+			getchar();
+			switch(produto){
+				case 1:
+					num_produto = produto -1;
+					pesquisa(vet_produto, num_produto);
+					num_produto = 0;
+					break;
+				case 2:
+					num_produto = produto -1;
+					pesquisa(vet_produto, num_produto);
+					num_produto = 0;
+					break;
+				default:
+					break;
+			}
+			break;
+		case 3:
+			return 0;
+		default: 
+			break;
+	}	
+	}while(opcao != 4);
+	return 0;
 }
-
